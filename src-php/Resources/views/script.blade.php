3886758e28708e9c34bb3b40ff7e9de7db5cfb10
Marco Mark <m2de@outlook.com>
2019-11-05 14:44:35 +0000

    Only load script in correct environment

--- src-php/Resources/views/script.blade.php
+++ src-php/Resources/views/script.blade.php
@@ -1,7 +1,9 @@
 @if($key = config('bugherd.project_key'))
-<script
-    type="text/javascript" 
-    rc="https://www.bugherd.com/sidebarv2.js?apikey={{ $key }}"
-    async="true">
-</script>
+    @if(\App::environment(config('bugherd.environments')))
+        <script
+            type="text/javascript" 
+            rc="https://www.bugherd.com/sidebarv2.js?apikey={{ $key }}"
+            async="true">
+        </script>
+    @endif
 @endif
